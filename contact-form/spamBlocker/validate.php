0413909b13f29860df2b51865856afaa04976aed
Kevin Marmet <kmarmet1@gmail.com>
2018-06-07 15:07:48 -0400

    Update validate.php

--- contact-form/spamBlocker/validate.php
+++ contact-form/spamBlocker/validate.php
@@ -2,7 +2,7 @@
    session_start();
    // Google reCaptcha
    $response   = $_POST['response'];
-   $secret_key = 'GOOGLE RECAPTCHA SECRETY KEY';
+   $secret_key = 'GOOGLE RECAPTCHA SECRET KEY';
    $verify     = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret_key}&response={$response}"));
 
    // CSRF Token
@@ -25,7 +25,6 @@
    $error         = '';
 
    // Helpers
-   $required_field_counter = 0;
    $redirect               = '';
    $required_fields        = array(
       'First Name'      => $first_name,
@@ -60,9 +59,6 @@
    foreach($required_fields as $field => $value) {
       $required_field_counter++;
       if (empty($value)) {
-         if ($required_field_counter < 2) {
-            $error .= "<h3>Missing:</h3>";
-         }
          $error .= "<p>&#8226; {$field}</p>";
       }
    }
@@ -106,94 +102,8 @@
    }
 
    // IF NO ERRORS -> SEND EMAIL
-   if (strlen($error) <= 0 && strlen($redirect) <=0) {
-      $mail_body = "
-        <table width='600' border='0' cellspacing='0' cellpadding='5'>
-            <tr>
-                <td valign='top' width='250'><strong>My inquiry is in reference to:</strong></td>
-                <td>$input_array</td>
-            </tr>
-            <tr style='background-color: #d7d7d7;'>
-               <td width='250'><strong>First Name:</strong></td>
-               <td>$first_name</td>
-            </tr>
-            <tr>
-               <td width='250'><strong>Last Name:</strong></td>
-               <td>$last_name</td>
-            </tr>
-            <tr style='background-color: #d7d7d7;' >
-               <td width='250'><strong>Title:</strong></td>
-               <td>$title</td>
-            </tr>
-            <tr>
-               <td width='250'><strong>Company:</strong></td>
-               <td>$company</td>
-            </tr>
-            <tr style='background-color: #d7d7d7;' >
-               <td valign='top'><strong>Address:</strong></td>
-               <td>$address_one</td>
-            </tr>
-            <tr>
-               <td valign='top'><strong>Address Two:</strong></td>
-               <td>$address_two</td>
-            </tr>
-            <tr style='background-color: #d7d7d7;' >
-               <td width='250'><strong>City / Province:</strong></td>
-               <td>$city</td>
-            </tr>
-            <tr>
-               <td width='250'><strong>State / Province:</strong></td>
-               <td>$state</td>
-            </tr>
-            <tr style='background-color: #d7d7d7;' >
-               <td width='250'><strong>Zip / Postal Code:</strong></td>
-               <td>$zip</td>
-            </tr>
-            <tr>
-               <td width='250'><strong>Country:</strong></td>
-               <td>$country</td>
-            </tr>
-            <tr style='background-color: #d7d7d7;' >
-               <td width='250'><strong>Email:</strong></td>
-               <td>$email</td>
-            </tr>
-            <tr>
-               <td width='250'><strong>Phone:</strong></td>
-               <td>$phone</td>
-            </tr>
-        </table> ";
-
-      // SERVER SETTINGS
-      require_once '../PHPMailer/PHPMailerAutoload.php';
-      $mail = new PHPMailer();
-      // $mail->SMTPDebug 					= 2;
-      $mail->From     = $email;
-      $mail->FromName = "{$first_name} {$last_name}";
-      $mail->Host     = 'HOST';
-      $mail->Mailer   = "smtp";
-
-      // RECIPIENTS
-      $mail->addAddress('email@email.com');
-      $mail->addBCC('email@email.com');
-      $mail->addCC('email@email.com');
-      $mail->addReplyTo($email, $first_name);
-
-      // CONTENT
-      $mail->Subject = 'Web Inquiry';
-      $mail->Body    = $mail_body;
-      $mail->AltBody = $mail_body;
-
-      // ATTACHMENTS
-      // $mail->addAttachment('/var/tmp/file.tar.gz');
-      // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');
-
-      if (!$mail->Send()) {
-         echo "<p>&#8226; Error sending mail: {$mail->ErrorInfo} Please try again</p>";
-      }
-      else {
-         $mail->ClearAddresses();
-         $mail->ClearAttachments();
-      }
+   if (strlen($error) <= 0 && strlen($redirect) <= 0) {
+      require_once '../send-email.php';
    }
 
    // Pass to front end
